38994fa9cfb4e0895d51259d7c36b7e91a3bbd34
Won Kyu Jeong <wjeong@csumb.edu>
2021-03-04 11:43:21 -0800

    Revert "React home/login pages"
    This reverts commit eff884b35090e6e53b541a624a712d86bbea4425.

--- src/index.js
+++ src/index.js
@@ -2,36 +2,13 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import Login from "./Login"
-import {
-  BrowserRouter as Router,
-  Switch,
-  Route,
-  Link
-} from "react-router-dom";
 import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
-  
-  <Router>
-  <div>
-    <nav>
-      <Link to="/App">Home</Link>  &nbsp;  &nbsp;
-      <Link to="/Login">Login</Link>  &nbsp;  &nbsp;
-      <Link to="/MyWishlist">My List</Link>  &nbsp;  &nbsp;
-    </nav>
-    <Switch>
-    <Route path="/App">
-        <App />
-      </Route>
-      <Route path="/Login">
-        <Login />
-      </Route>
-    </Switch>
-  </div>
-</Router>,
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
   document.getElementById('root')
-
 );
 
 // If you want to start measuring performance in your app, pass a function
