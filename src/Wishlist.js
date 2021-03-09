7ae34c2827ddb0a84fa2ef2b6b9ff568959e841b
Joseph Newman <josephnew900@gmail.com>
2021-03-09 13:11:32 -0800

    Create Wishlist.js
    wishlist update

---
+++ src/Wishlist.js
@@ -0,0 +1,15 @@
+import React from "react";
+import NavBar from "./NavBar";
+function Wishlist() {
+  return (
+    <div style={{ textAlign: "center" }}>
+      <NavBar></NavBar>
+      <br></br>
+      <br></br>
+      <h3>This is wishlist</h3>
+    </div>
+  );
+}
+
+export default Wishlist;
+
