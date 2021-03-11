8e317b87e204a41c4ce6ab1557735957c07d7fe7
Joseph Newman <josephnew900@gmail.com>
2021-03-10 21:23:06 -0800

    Uploading test from backend -> front end; includes files need for server -> website
    This was a test from the back end to the front end, uploading as an example, as well as pushing proxy code in package-lock for connection to backend

--- src/Wishlist.js
+++ src/Wishlist.js
@@ -1,12 +1,20 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import NavBar from "./NavBar";
 function Wishlist() {
+  const [currentTime, setCurrentTime] = useState(0);
+
+  useEffect(() => {
+    fetch('/time').then(res => res.json().then(data => {
+      setCurrentTime(data.time);
+    }))
+  },[])
   return (
     <div style={{ textAlign: "center" }}>
-      <NavBar></NavBar>
+      <NavBar/>
       <br></br>
       <br></br>
       <h3>This is wishlist</h3>
+      <h3>Message from server : {currentTime}</h3>
     </div>
   );
 }
