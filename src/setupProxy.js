a7a6e30e22a0572db3c76f5cd43fd4eb5ada7ae4
Ike Hirzel <hirzelisaac@gmail.com>
2021-03-13 21:49:54 -0800

    Changed ports of routes of react

--- src/setupProxy.js
+++ src/setupProxy.js
@@ -2,9 +2,9 @@ const { createProxyMiddleware } = require('http-proxy-middleware');
 
 module.exports = function(app)
 {
-    app.use(createProxyMiddleware("/api/test", { target: "http://localhost:5000" }));
-	app.use(createProxyMiddleware("/api/item", { target: "http://localhost:5000" }));
-	app.use(createProxyMiddleware("/api/list", { target: "http://localhost:5000" }));
-	app.use(createProxyMiddleware("/api/user", { target: "http://localhost:5000" }));
-	app.use(createProxyMiddleware("/api/shaq", { target: "http://localhost:5000" }));
+    app.use(createProxyMiddleware("/api/test", { target: "http://localhost:8000" }));
+	app.use(createProxyMiddleware("/api/item", { target: "http://localhost:8000" }));
+	app.use(createProxyMiddleware("/api/list", { target: "http://localhost:8000" }));
+	app.use(createProxyMiddleware("/api/user", { target: "http://localhost:8000" }));
+	app.use(createProxyMiddleware("/api/shaq", { target: "http://localhost:8000" }));
 };
