d8b0dd39b20acee22cc4599bc09e71e8e46fd5e1
Joseph Newman <josephnew900@gmail.com>
2021-03-24 22:47:36 -0700

    added edit profile page and functionality

---
+++ public/js/updateprofile.js
@@ -0,0 +1,31 @@
+let user = sessionStorage.getItem('user');
+let pass = sessionStorage.getItem('pass');
+
+function saveUserChange(){
+    var username = document.getElementById('usernameEdit').value;
+    var password = document.getElementById('passwordEdit').value;
+    if(pass == password){
+        let api = new Api(user,pass);
+        api.update_user(username, password);
+        console.log("updated");
+        location.href = "/login";
+    }
+}
+function changeUserPass(){
+    var oldPass = document.getElementById('currpassword').value;
+    var newPass = document.getElementById('newpassword').value;
+    console.log("test");
+    if(oldPass == pass){
+        console.log("here");
+        let api = new Api(user,pass);
+        api.update_user(user, newPass);
+        console.log("updated");
+        location.href = "/login";
+    }
+}
+
+function deleteAccount(){
+    let api = new Api(user,pass);
+    api.deleteAccount();
+    location.href = "/login";
+}
