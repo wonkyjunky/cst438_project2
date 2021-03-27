42d15e418147d68a11979eee59de8a94ef86aca7
Ike Hirzel <hirzelisaac@gmail.com>
2021-03-26 23:48:19 -0700

    Fixed formatting of html files, removed references to css files. Moved valid password into its own file so it didn't have to be duplicated

--- public/js/login.js
+++ public/js/login.js
@@ -17,31 +17,6 @@ $(async () =>
 });
 
 /**
- * Checks if a password meets criteria
- * 
- * @param	password	the password to examine
- * 
- * @return	bool value, true if password is valid
- */
-function validpassword(password)
-{
-	if (password.length < 6) return false;
-	// special characters that are allowed in passwords
-	let specials = "`~!@#$%^&*()-_=+[{]}\\|'\":;?/>.<,";
-
-	for (let i = 0; i < password.length; i++) {
-		for (let j = 0; j < specials.length; j++) {
-			// check every password character for special as at least one is required
-			if (password[i] == specials[j]) {
-				return true;
-			}
-		}
-	}
-	// no specials found, invalid password
-	return false;
-}
-
-/**
  * Handles input on login page
  * 
  * @param	type	type of user action (login or account creation)
